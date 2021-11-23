81d574729903dcfbcd1f2f15d3e0fabb11f7fca7
Stefan Probst <stefanprobst@kolabnow.com>
2021-11-23 15:27:39 +0100

    chore: move to js and add types so config can be imported in ts

---
+++ index.js
@@ -0,0 +1,30 @@
+const config = {
+  printWidth: 100,
+  proseWrap: "always",
+  semi: false,
+  singleQuote: true,
+  trailingComma: "all",
+  overrides: [
+    {
+      files: ["content/**/*.@(md|mdx)"],
+      options: {
+        proseWrap: "always",
+        singleQuote: false,
+      },
+    },
+    {
+      files: ["content/**/*.yml"],
+      options: {
+        singleQuote: false,
+      },
+    },
+    {
+      files: ["*.svg"],
+      options: {
+        parser: "html",
+      },
+    },
+  ],
+};
+
+module.exports = config;
